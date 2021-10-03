8070285ab349815e3338343e701088554aa605ce
SpencerDamon <90030235+SpencerDamon@users.noreply.github.com>
2021-10-03 14:55:29 -0700

    Update MainActivity.kt
    Removed initial val that would not compile.
    Declared a private residents property to be inherited by subclasses.
    Added capacity property for Dwellings class.
    Added hasRoom function which returns a Boolean value comparing residents to capacity.
    Created a object instance of SquareCabin, passing the resident parameter, and calling the constructor on the Dwelling object instance.

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,11 +1,26 @@
 main(){
-  //Makes an object instance from the Dwelling class, this line will not compile because you cannot create an instance from an abstract class!
-  val dwelling = Dwelling()
+
   //created a root class of Dwelling; the class must be astract, because variable will remain undefined. It cannot be instantiated because it is not fully implemented.
-  abstract class Dwelling(){
+  //Defined the residents property to be inherited and used by all other children classes.
+  //private declares the property to only been seen or used by this class, and inaccessable to the rest of the program.
+  abstract class Dwelling(private var residents: Int){
   
     //abstract because undefined in parent class, not initialized.
     //All dwellings require a building material of some kind.
     abstract val buildingMaterial: String
+    
+    //The capacity property changes for different dwellings therefore it can only be defined in subclasses of the Dwelling root class.
+    abstract val capacity: Int
+    
+    //Returns a Boolean value to where it is called; residents must be limited by capacity.
+    fun hasRoom() : Boolean {
+      return residents < capacity
+    
+    }
   }
+  //Creates a class and object instance; a colon indicates an inheritance relationship to parent class, followed by a call to initialize the parent Dwelling and calling its constructor.
+  //Must pass the required parameters ezpected by the superclass could be fized Dwelling(3), to be more flezible pass the private variable residents.
+  //shorthand for class SquareCabin constructor(residents: Int)...
+  //
+  class SquareCabin(residents: Int) : Dwelling(residents)
 }
